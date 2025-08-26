ab6294bf5c7c7626023f031d22277519d724e406
Allen Alcabaza <alcabaza2tallen@gmail.com>
2025-08-26 22:43:00 +0800

    9.Palindrome-numbers.cs

---
+++ easy/9.palindrome-number.cs
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=9 lang=csharp
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+public class Solution {
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        int reversed = 0;
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == reversed || x == reversed / 10;
+    }
+}
+// @lc code=end
+
